71b10340e9a50f232f6f184447be310a0d3b92e1
FJ98 <felix.solano@utec.edu.pe>
2019-08-24 20:51:55 -0500

    jumping-mario

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-
-using namespace std;
+#include <bits/stdc++.h>
 
 int main() {
-	return EXIT_SUCCESS;
-}
+    int testCases = 0, numberOfWalls = 0;
+    std::cin >> testCases;
 
+    for (int i = 1; i <= testCases; ++i) {
+        std::cin >> numberOfWalls;
+        int arr[numberOfWalls];
+        int currentJump = 0, highJump = 0, lowJump = 0;
+
+        for (int j = 0; j < numberOfWalls; ++j) {
+            std::cin >> currentJump;
+            arr[j] = currentJump;
+
+            if (j == 0) continue;
+            if (arr[j] > arr[j - 1]) ++highJump;
+            if (arr[j] < arr[j - 1]) ++lowJump;
+        }
+
+        std::cout << "Case " << i << ": " << highJump << " " << lowJump << std::endl;
+    }
+
+    return 0;
+}
